7a128806809aa61753cd2566e017bed95b72cdfe
Arnav Srivastava <arnavsrivastava@college.harvard.edu>
2021-12-20 15:30:12 -0500

    Updating space sol, more concise syntax

--- linked-list-cycle/O(1)-space-sol.cpp
+++ linked-list-cycle/O(1)-space-sol.cpp
@@ -16,16 +16,18 @@ public:
             return false;
         }
         // walker traverses one node at a time
-        ListNode walker = head;
+        ListNode *walker = head;
         // runner traverses two nodes at a time
-        ListNode runner = head;
+        ListNode *runner = head;
         
         // if there exists a cycle in our list, then runner will never be null
         // no need for memory, runner will cycle back and equal walker at some point
-        while(runner.next!=null && runner.next.next!=null) {
-            walker = walker.next;
-            runner = runner.next.next;
-            if(walker==runner) return true;
+        while((runner->next) != NULL && runner->next->next != NULL) {
+            walker = walker->next;
+            runner = runner->next->next;
+            if (walker == runner) {
+                return true;
+            }
         }
         // if we ever reach a node with no next link upcoming, no cycle!
         return false;
